e0c3d26440a7110ddfacb325505af261fdc72b9d
Wellington Gomes <47453049+WellingtonRSGomes@users.noreply.github.com>
2022-10-21 23:48:19 -0300

    Create menu bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import { CreateMenuBar } from './styles'
+import { CreateMenuBar, CreateBackground, SocialMedia } from './styles'
+import styled from 'styled-components'
 
 function App() {
   return (
-    <CreateMenuBar></CreateMenuBar>
+    <div>
+      <CreateBackground></CreateBackground>
+
+      <Container>
+        <CreateMenuBar></CreateMenuBar>
+        <SocialMedia></SocialMedia>
+      </Container>
+    </div>
   )
 }
+
+const Container = styled.main`
+  max-width: 1024px;
+  margin: 0 auto;
+`;
 
 export default App
--- src/styles.tsx
+++ src/styles.tsx
@@ -1,47 +1,91 @@
 import styled from 'styled-components'
-import userImg from './img/userIcon.jpg'
+import userImg from './img/userIcon.png'
+import backgroundImg from './img/background.jpg'
+import { GithubLogo, LinkedinLogo, WhatsappLogo } from 'phosphor-react'
+
+const ContentDisplay = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export function CreateBackground() {
+  return (
+    <ImgBackground src={backgroundImg}></ImgBackground>
+  )
+}
+
+const ImgBackground = styled.img`
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -1;
+  width: 100%;
+  height: 85vh;
+`;
 
 export function CreateMenuBar() {
   return (
-    <MenuBar>
-      <div>
-        <ImageProps src={userImg}></ImageProps>
-      </div>
-      
-      <div>
-        <ul>
-          <li>Work</li>
-          <li>Services</li>
-          <li>About</li>
-        </ul>
-      </div>
-    </MenuBar>
+      <ContentDisplay>
+        <ImgDiv>
+          <ImageProps src={userImg}></ImageProps>
+        </ImgDiv>
+        
+        <nav>
+          <UlMenu>
+            <LiOption>Work</LiOption>
+            <LiOption>Services</LiOption>
+            <LiOption>About</LiOption>
+          </UlMenu>
+        </nav>
+      </ContentDisplay>
   )
 }
 
-const MenuBar = styled.div`
-  background: transparent;
-  align-items: center;
+const ImgDiv = styled.div`
+  border-radius: 50%;
   display: flex;
-  justify-content: space-around;
-
-  .img{
-    
-  }
-
-  .ul{
-    
-  }
+  align-items: center;
+  justify-content: center;
+  background: pink;
+  padding: 10px 18px;
 `;
 
 const ImageProps = styled.img`
-  border-radius: 50%;
-  width: 130px;
-  height: 130px;
+  height: 50px;
 `;
 
-function CreateOptions(){
+const UlMenu = styled.ul`
+  display: flex;
+  aling-items: center;
+  column-gap: 30px;
+`;
+
+const LiOption = styled.li`
+  list-style-type: none;
+  color: #FFF;
+`;
+
+export function SocialMedia(){
   return(
-    
+    <ContentDisplay>
+      <SmDiv>
+        <GithubLogo size={51} weight="fill" color="#FFF" />
+        <LinkedinLogo size={51} weight="fill" color="#FFF" />
+        <WhatsappLogo size={51} weight="fill" color="#FFF" />
+      </SmDiv>
+
+      <div>
+        <h1>I'M LUCAS</h1>
+        <h2>FULLSTACK DEVELOPER</h2>
+      </div>
+    </ContentDisplay>
   )
 }
+
+const SmDiv = styled.div`
+  margin-top: 90px;
+  display: flex;
+  flex-direction: column;
+  row-gap: 20px;
+`;
